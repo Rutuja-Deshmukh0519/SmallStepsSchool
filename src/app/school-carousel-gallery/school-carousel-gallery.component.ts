408ab2f7fa0152cdd3508a461e05746151bd88ae
Rutuja-Deshmukh0519 <60124635+Rutuja-Deshmukh0519@users.noreply.github.com>
2023-01-19 23:17:14 +0530

    All components working

--- src/app/school-carousel-gallery/school-carousel-gallery.component.ts
+++ src/app/school-carousel-gallery/school-carousel-gallery.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-school-carousel-gallery',
   templateUrl: './school-carousel-gallery.component.html',
   styleUrls: ['./school-carousel-gallery.component.css']
 })
-export class SchoolCarouselGalleryComponent {
+export class SchoolCarouselGalleryComponent{
 
+  
 }
